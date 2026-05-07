9ee3ef8be8e0462e0e723ca894851ed8f4e84596
Bikram <algo.bikram@gmail.com>
2026-05-07 22:48:49 +0530

    swap program

---
+++ swaplogic.py
@@ -0,0 +1,16 @@
+#number of ways to swap 2 values 
+#swaplogic1
+a=input("Enter the first value of a: ")
+b=input("Enter the first value of b: ")
+print("="*20)
+print("Before swapping: ")
+print("a: ",a)
+print("b: ",b)
+print("="*20)
+#swapping logic
+temp=a
+a=b
+b=temp
+print("After swapping:)")
+print("a: ",a)
+print("b: ",b)
